528bd734c08908caf4c4da91525bea77e94bd302
Gennady Alheev <44393437+MiiMan@users.noreply.github.com>
2024-07-20 22:24:16 +0300

    Updated: hide table of contents

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -189,7 +189,6 @@ export const NotionPage: React.FC<types.PageProps> = ({
   const isBlogPost =
     block?.type === 'page' && block?.parent_table === 'collection'
 
-  const showTableOfContents = !!isBlogPost
   const minTableOfContentsItems = 3
 
   const pageAside = React.useMemo(
